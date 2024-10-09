fd2038985b456ffabe90026099e7cc7e4477eaa9
Mayra Castrosqui Florian <macastro@c1r9s4.42madrid.com>
2024-10-09 19:16:01 +0200

    exit args supported

--- src/4_builtin/exit.c
+++ src/4_builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: macastro <macastro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 10:52:33 by msoriano          #+#    #+#             */
-/*   Updated: 2024/10/09 11:53:07 by macastro         ###   ########.fr       */
+/*   Updated: 2024/10/09 19:05:32 by macastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,28 @@
 
 int	exec_exit(t_cmdnode node, t_shcontext *env)
 {
+	int	st;
+
+	if (node.argc != 1 && node.argc != 2)
+	{
+		my_perror("exit\nbash: exit: too many arguments");
+		return (1);
+	}
+	if (node.argc == 2)
+	{
+		if (ft_atoi_secure(node.argv[1], &st))
+			st = st % 255;
+		else
+		{
+			my_perror("error exit: numeric argument required");
+			return (2);
+		}
+	}
+	else
+		st = env->status;
 	ft_free_arrstr(env->env);
 	free_node(node);
 	if (env->nopipe)
 		ft_printf("exit\n");
-	exit(env->status);
+	exit(st);
 }
